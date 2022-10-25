c260a46ed0956d23321c4dad76860cc92b818287
azuma <azuma20011224@gmail.com>
2022-10-26 04:06:18 +0900

    create urls

---
+++ backend/api/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+app_name = 'user'
+
+router = DefaultRouter()
+router.register('profile', views.ProfileviewSet)
+router.register('post', views.PostViewSet)
+router.register('comment', views.CommentViewSet)
+
+urlpatterns = [
+    path('register/', views.CreateUserView.as_view(), name='register'),
+    path('myprofile/', views.MyProfileListView.as_view(), name='myprofile'),
+    path('profile-list/', views.ProfileListView.as_view(), name='profile-list'),
+    path('profile-detail/<slug:pk>/',
+         views.ProfileRetrieveView.as_view(), name='profile-detail'),
+    path('post-list/', views.PostListView.as_view(), name='post-list'),
+    path('post-detail/<slug:pk>/',
+         views.PostRetrieveView.as_view(), name='post-detail'),
+    path('', include(router.urls))
+]
